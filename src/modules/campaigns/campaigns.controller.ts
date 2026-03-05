1d39a9ab7f38e78bc4102346375f298a578cd5c2
Trọng Lê <ductrong03ldt@gmail.com>
2026-03-05 15:29:20 +0700

    add campaigns response message

--- src/modules/campaigns/campaigns.controller.ts
+++ src/modules/campaigns/campaigns.controller.ts
@@ -7,31 +7,37 @@ import {
   UpdateCampaignReqDto,
 } from './campaigns.schema'
 import { UserId } from 'src/common/decorators/user-id.decorator'
+import { ResponseMessage } from 'src/common/decorators/response-message.decorator'
 
 @Controller('campaigns')
 export class CampaignsController {
   constructor(private readonly campaignsService: CampaignsService) {}
 
+  @ResponseMessage('Lấy danh sách campaign thành công')
   @Get()
   list(@UserId() userId: string, @Query() query: GetCampaignQueryDto) {
     return this.campaignsService.list(userId, query)
   }
 
+  @ResponseMessage('Tạo campaign thành công')
   @Post()
   create(@UserId() userId: string, @Body() body: CreateCampaignReqDto) {
     return this.campaignsService.create(userId, body)
   }
 
+  @ResponseMessage('Cập nhật campaign thành công')
   @Patch(':id')
   update(@UserId() userId: string, @Body() body: UpdateCampaignReqDto, @Param('id') campaignId: string) {
     return this.campaignsService.update({ campaignId, userId }, body)
   }
 
+  @ResponseMessage('Xóa campaign thành công')
   @Delete(':id')
   delete(@UserId() userId: string, @Param('id') campaignId: string) {
     return this.campaignsService.delete({ campaignId, userId })
   }
 
+  @ResponseMessage('Gửi campaign thành công')
   @Post(':id/send')
   startCampaign(@UserId() userId: string, @Body() body: StartCampaignReqDto, @Param('id') campaignId: string) {
     return this.campaignsService.startEmailCampaign({
